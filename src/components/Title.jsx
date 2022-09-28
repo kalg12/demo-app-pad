71a30083b4fe2636c00fbe5c8154e5ffe613e0ee
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-09-27 21:32:31 -0500

    Uso de map

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
-const Title = (props) => {
+const Title = () => {
+  const array = [
+    {
+      nombre: 'Kevin',
+      apellido: 'Luciano',
+      edad: 27
+    },
+    {
+      nombre: 'Juan',
+      apellido: 'Perez',
+      edad: 30
+    },
+    {
+      nombre: 'Pedro',
+      apellido: 'Gomez',
+      edad: 25
+    },
+    {
+      nombre: 'Maria',
+      apellido: 'Gomez',
+      edad: 25
+    },
+    {
+      nombre: 'Maria',
+      apellido: 'Gomez',
+      edad: 25
+    },
+    {
+      nombre: 'Maria',
+      apellido: 'Gomez',
+      edad: 25
+    },
+    {
+      nombre: 'Maria',
+      apellido: 'Gomez',
+      edad: 25
+    },
+    {
+      nombre: 'Maria',
+      apellido: 'Gomez',
+      edad: 25
+    }
+  ]
+
+/*   const disparaUnAlert = () => {
+    alert("Hola")
+  } */
+
+/*   useEffect(() => {
+    disparaUnAlert()
+  }) */
+
   return (
     <>
-    <h1>Datos del coach</h1>
-    <h4>Nombre del coach: {props.nombre}</h4>
-    <h4>Apellido del coach: {props.apellido}</h4>
-    <h4>Edad del coach: {props.edad}</h4>
+      <ul>
+        {array.map((listado, index) => {
+          return (
+            <li key={index}>{listado.nombre} {listado.apellido} {listado.edad}</li>
+          )
+        })}
+      </ul>
     </>
   )
 }
