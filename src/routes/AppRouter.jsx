98830a357f60fb53e688997ee6489fbee896609c
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-09-30 00:26:23 -0500

    GitHub pages and blank page error

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import CrudPage from '../pages/CrudPage';
 import HomePage from '../pages/HomePage';
 import UcampersPage from '../pages/UcampersPage';
